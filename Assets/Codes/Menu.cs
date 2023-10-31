02506809889e0c68852f172d7dfd916cc2b8ef85
Kiana <114351440+Kianaquasary@users.noreply.github.com>
2023-10-31 05:21:38 +0300

    First Commit

---
+++ Assets/Codes/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Exit ();	
+		}
+
+	}
+
+
+
+
+	public void Exit(){
+		Application.Quit ();
+	}
+
+
+
+
+}
---
+++ Assets/Codes/TriggerText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerText : MonoBehaviour {
+
+	public GameObject bio;
+
+	void Start () {
+		bio.SetActive (false);
+	}
+
+
+
+	private void OnTriggerStay(Collider Other){
+		bio.SetActive (true);
+	}
+
+	private void OnTriggerExit(Collider Other){
+		bio.SetActive (false);
+	}
+
+}
